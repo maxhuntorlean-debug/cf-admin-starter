34d1a0ed116697710cca9711a330c658ad3ba123
maxhuntorlean-debug <maxhuntorlean@gmail.com>
2026-08-15 11:28:26 +0200

    Remove POS branding from admin home

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -1,16 +1,8 @@
-export function HomePage(
-    user
-) {
-
+export function HomePage(user) {
     return `
         <div class="adminPage">
-
             <header class="adminHeader">
-
-                <div>
-                    🐼 Котопанда POS Admin
-                </div>
-
+                <div>⚙️ Admin Panel</div>
                 <div>
                     ${escapeHtml(
                         user.name ||
@@ -18,78 +10,32 @@ export function HomePage(
                         ""
                     )}
                 </div>
-
             </header>
 
-
             <main class="adminMenu">
-
-                <button
-                    id="openUsers"
-                    class="adminMenuItem"
-                    type="button"
-                >
+                <button id="openUsers" class="adminMenuItem" type="button">
                     👥 Пользователи
                 </button>
 
-
-                <button
-                    id="openRoles"
-                    class="adminMenuItem"
-                    type="button"
-                >
+                <button id="openRoles" class="adminMenuItem" type="button">
                     🛡 Роли и права
                 </button>
 
-
-                <button
-                    id="logoutButton"
-                    class="adminMenuItem"
-                    type="button"
-                >
+                <button id="logoutButton" class="adminMenuItem" type="button">
                     🚪 Выйти
                 </button>
-
             </main>
-
         </div>
     `;
 }
 
-
-export function initHome({
-    onUsers,
-    onRoles,
-    onLogout
-}) {
-
-    document
-        .getElementById("openUsers")
-        .addEventListener(
-            "click",
-            onUsers
-        );
-
-
-    document
-        .getElementById("openRoles")
-        .addEventListener(
-            "click",
-            onRoles
-        );
-
-
-    document
-        .getElementById("logoutButton")
-        .addEventListener(
-            "click",
-            onLogout
-        );
+export function initHome({ onUsers, onRoles, onLogout }) {
+    document.getElementById("openUsers").addEventListener("click", onUsers);
+    document.getElementById("openRoles").addEventListener("click", onRoles);
+    document.getElementById("logoutButton").addEventListener("click", onLogout);
 }
 
-
 function escapeHtml(value) {
-
     return String(value ?? "")
         .replaceAll("&", "&amp;")
         .replaceAll("<", "&lt;")
